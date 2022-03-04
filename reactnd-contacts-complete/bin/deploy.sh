6d9d2492327807a45029acbaf2f99031819f5a71
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-03-04 20:05:01 +0200

    Editing the pipline

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,3 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+aws s3 mb s3://ammar-massoud-ud --region us-west-1
+
+aws s3 cp --recursive --acl public-read ./build s3://ammar-massoud-ud/
